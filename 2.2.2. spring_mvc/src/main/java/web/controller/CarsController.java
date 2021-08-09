c5906fd3c7ec2d2f1bda1eaa4698143982d8ff60
alandrnov <al.andr.nov@gmail.com>
2021-08-09 15:00:48 +0300

    CarDAO interface added

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -6,17 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.DAO.CarsDAO;
-import web.models.Car;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import web.DAO.CarsDAOImpl;
 
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
     @Autowired
-    private CarsDAO carsDAO;
+    private CarsDAOImpl carsDAO;
 
     @GetMapping
     public String cars(@RequestParam(value = "count",required = false) Integer count, Model model) {
